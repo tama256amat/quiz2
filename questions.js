fb1d3d5bd00d850f24fd47c1bff7674ebb21323b
tama256amat <aisudog@hotmail.com>
2021-12-31 17:21:07 +0900

    正解時画面遷移は完了しました。正解数表示はもうしこしです。

--- questions.js
+++ questions.js
@@ -1,8 +1,28 @@
-//
+// //
 
-//「正解」「残念」によって、explanation.html の画像を変える
+// //「正解」「残念」によって、explanation.html の画像を変える
 
-// let changeImg = document.getElementById("imgurl");
+// document.getElementById("ans_ok").addEventListener("click", () => {
+//   document.getElementById("imgUrl");
+//   imgUrl.src =
+//     "https://user-images.githubusercontent.com/91357435/147396635-edb9e813-3ee3-418c-8029-aadea84807b2.png";
+// });
+
+// document.getElementById("ans_ng1").addEventListener("click", () => {
+//   location.href = "explanation.html";
+//   document.getElementById("imgUrl");
+//   imgUrl.src =
+//     "https://user-images.githubusercontent.com/91357435/147396644-f956b12a-179d-4795-b21d-b2c79478b62a.png";
+// });
+
+// document.getElementById("ans_ng2").addEventListener("click", () => {
+//   location.href = "explanation.html";
+//   document.getElementById("imgUrl");
+//   imgUrl.src =
+//     "https://user-images.githubusercontent.com/91357435/147396644-f956b12a-179d-4795-b21d-b2c79478b62a.png";
+// });
+
+// let changeImg = document.getElementById("i");
 // changeImg.src =
 //   "https://user-images.githubusercontent.com/91357435/147396635-edb9e813-3ee3-418c-8029-aadea84807b2.png";
 // function judgeImg(changeImg) {
@@ -26,22 +46,10 @@
 
 // }
 
-//正解数を集計
+// //正解数を集計
 
-function onClickCorrect() {
-  location.href = "explanation.html";
-  const point = localStorage.getItem("point")
-    ? parseInt(localStorage.getItem("point"))
-    : 0;
-  localStorage.setItem("point", point + 1);
-}
-
-// 最終問題解説ページからresult.html へ 。クリックしたら正解数出るようにしたいけど。。。
-document.getElementById("load").addEventListener("click", () => {
-  location.href = "result.html";
-});
-
-//result.html の点数の場所に反映
-function askForResult() {
-  const total = sessionStorage.getItem("point");
+function onClickCorrect(number) {
+  location.href = "explanation" + number + ".html";
+  const point = localStorage.getItem("point")? parseInt(localStorage.getItem("point")): 0;
+  localStorage.setItem("point", "0");
 }
