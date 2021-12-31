7d0c7cc81cbee400911f351b80b6fba0269aeadf
tama <91357435+tama256amat@users.noreply.github.com>
2021-12-31 10:47:06 +0900

    Revert "様々変更しました。いったんマージしますが、取り消すかもしれないです。"

--- questions.js
+++ questions.js
@@ -1,20 +1,47 @@
-window.onload = function(){
+//
 
-const data = location.href.split("?")[1];
-const getQ = data.substring(0,2);
-// const ttlQ = data.substring(2,2);
-// const ansQ = data.substring(4,2); 
+//「正解」「残念」によって、explanation.html の画像を変える
 
-document.getElementById("ans_ok".onclick) = function(){
-    location.href = "explamnation" + getQ + ".html?" + data + "01";
+// let changeImg = document.getElementById("imgurl");
+// changeImg.src =
+//   "https://user-images.githubusercontent.com/91357435/147396635-edb9e813-3ee3-418c-8029-aadea84807b2.png";
+// function judgeImg(changeImg) {
+
+//ねこせいかい
+// let answer = "true"
+// if (true) {https://user-images.githubusercontent.com/91357435/147396635-edb9e813-3ee3-418c-8029-aadea84807b2.png
+
+//     }
+//   //ねこざんねん
+//     else {
+//         changeImg.src ="https://user-images.githubusercontent.com/91357435/147396644-f956b12a-179d-4795-b21d-b2c79478b62a.png";
+//   }
+// }
+// const answersList = document.querySelectorAll  button-container
+// function checkKlickedanswer(){
+
+//     解答のリンク先は同じ
+//     //ボタンによって答えを判定 ページに遷移
+//     let explanationPageLink;
+
+// }
+
+//正解数を集計
+
+function onClickCorrect() {
+  location.href = "explanation.html";
+  const point = localStorage.getItem("point")
+    ? parseInt(localStorage.getItem("point"))
+    : 0;
+  localStorage.setItem("point", point + 1);
 }
-document.getElementById("ans_ng1".onclick) = function(){
-    location.href = "explamnation" + getQ + ".html?" + data + "02";
+
+// 最終問題解説ページからresult.html へ 。クリックしたら正解数出るようにしたいけど。。。
+document.getElementById("load").addEventListener("click", () => {
+  location.href = "result.html";
+});
+
+//result.html の点数の場所に反映
+function askForResult() {
+  const total = sessionStorage.getItem("point");
 }
-document.getElementById("ans_ng2".onclick) = function(){
-    location.href = "explamnation" + getQ + ".html?" + data + "02";
-    }
-}
-
-
-
